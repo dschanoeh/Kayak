6866649961a4eef73a6aee91cdc7b33714cc4613
Jan-Niklas Meier <dschanoeh@googlemail.com>
2011-04-17 16:48:41 +0200

    added ProjectManagementListener

---
+++ Kayak-ui/src/main/java/com/github/kayak/ui/projects/ProjectManagementListener.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.github.kayak.ui.projects;
+
+/**
+ *
+ * @author dschanoeh
+ */
+public interface ProjectManagementListener {
+
+    public void projectsUpdated();
+}
