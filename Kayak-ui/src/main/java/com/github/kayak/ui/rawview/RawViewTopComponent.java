8ca7836570149d0df73ce0a30d0f2ac749b26aa1
Jan-Niklas Meier <dschanoeh@googlemail.com>
2011-09-23 08:46:14 +0200

    binary hex and ascii data view in raw view

--- Kayak-ui/src/main/java/com/github/kayak/ui/rawview/RawViewTopComponent.java
+++ Kayak-ui/src/main/java/com/github/kayak/ui/rawview/RawViewTopComponent.java
@@ -127,16 +127,32 @@ public final class RawViewTopComponent extends TopComponent {
 
                     sb.append("ID: ");
                     sb.append(idString);
-                    sb.append(" | ");
+                    sb.append("\nBinary: ");
 
                     for(byte b : bytes) {
                         sb.append(Util.hexStringToBinaryString(Util.byteToHexString(b)));
                         sb.append(' ');
                     }
-                    sb.deleteCharAt(sb.length()-1);
-                    jTextField2.setText(sb.toString());
+                    sb.setLength(sb.length()-1);
+
+                    sb.append("\nHex:       ");
+                    for(byte b : bytes) {
+                        sb.append(Util.byteToHexString(b));
+                        sb.append("       ");
+                    }
+                    sb.setLength(sb.length()-7);
+
+                    sb.append("\nASCII:      ");
+                    for(byte b : bytes) {
+                        sb.append((char) b);
+                        sb.append("        ");
+                    }
+                    sb.setLength(sb.length()-8);
+
+
+                    jTextArea1.setText(sb.toString());
                 } else {
-                    jTextField2.setText("");
+                    jTextArea1.setText("");
                 }
             }
         }
@@ -198,9 +214,8 @@ public final class RawViewTopComponent extends TopComponent {
         jCheckBox1 = new javax.swing.JCheckBox();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jTextField2 = new javax.swing.JTextField();
-
-        setLayout(new javax.swing.BoxLayout(this, javax.swing.BoxLayout.Y_AXIS));
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
 
         jToolBar1.setFloatable(false);
         jToolBar1.setRollover(true);
@@ -242,8 +257,6 @@ public final class RawViewTopComponent extends TopComponent {
         });
         jToolBar1.add(jButton2);
 
-        add(jToolBar1);
-
         jPanel1.setLayout(new javax.swing.BoxLayout(jPanel1, javax.swing.BoxLayout.X_AXIS));
 
         org.openide.awt.Mnemonics.setLocalizedText(jLabel1, org.openide.util.NbBundle.getMessage(RawViewTopComponent.class, "RawViewTopComponent.jLabel1.text")); // NOI18N
@@ -263,10 +276,8 @@ public final class RawViewTopComponent extends TopComponent {
         });
         jPanel1.add(jCheckBox1);
 
-        add(jPanel1);
-
         jTable1.setAutoCreateRowSorter(true);
-        jTable1.setFont(new java.awt.Font("Monospaced", 0, 14));
+        jTable1.setFont(new java.awt.Font("Monospaced", 0, 14)); // NOI18N
         jTable1.setModel(model);
         jTable1.setDoubleBuffered(true);
         jTable1.getColumnModel().getColumn(0).setPreferredWidth(50);
@@ -276,12 +287,35 @@ public final class RawViewTopComponent extends TopComponent {
         jTable1.getColumnModel().getColumn(4).setPreferredWidth(160);
         jScrollPane1.setViewportView(jTable1);
 
-        add(jScrollPane1);
+        jTextArea1.setEditable(false);
+        jTextArea1.setFont(new java.awt.Font("Monospaced", 0, 11)); // NOI18N
+        jTextArea1.setLineWrap(true);
+        jTextArea1.setRows(4);
+        jTextArea1.setTabSize(4);
+        jTextArea1.setMinimumSize(new java.awt.Dimension(100, 200));
+        jTextArea1.setPreferredSize(new java.awt.Dimension(100, 200));
+        jScrollPane2.setViewportView(jTextArea1);
 
-        jTextField2.setEditable(false);
-        jTextField2.setFont(new java.awt.Font("Monospaced", 0, 11));
-        jTextField2.setText(org.openide.util.NbBundle.getMessage(RawViewTopComponent.class, "RawViewTopComponent.jTextField2.text")); // NOI18N
-        add(jTextField2);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 410, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 410, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 410, Short.MAX_VALUE)
+            .addComponent(jToolBar1, javax.swing.GroupLayout.DEFAULT_SIZE, 410, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 224, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
     }// </editor-fold>//GEN-END:initComponents
 
     private void jCheckBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox1ActionPerformed
@@ -332,9 +366,10 @@ private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRS
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
+    private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
     private javax.swing.JToggleButton jToggleButton1;
     private javax.swing.JToolBar jToolBar1;
     // End of variables declaration//GEN-END:variables
