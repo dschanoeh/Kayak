3512b91da03aca6692098bd5ca57d8c5ad383349
Jan-Niklas Meier <dschanoeh@googlemail.com>
2011-12-14 11:07:19 +0100

    Changed default port in manual connection

--- Kayak-ui/src/main/java/com/github/kayak/ui/connections/NewConnectionAction.java
+++ Kayak-ui/src/main/java/com/github/kayak/ui/connections/NewConnectionAction.java
@@ -1,6 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * 	This file is part of Kayak.
+ *
+ *	Kayak is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Lesser General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Kayak is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Lesser General Public License
+ *	along with Kayak.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 package com.github.kayak.ui.connections;
@@ -22,7 +35,7 @@ public final class NewConnectionAction implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent ev) {
-        String url = JOptionPane.showInputDialog("Please type in a socket to connect with.", "socket://can0@192.168.30.129:28640");
+        String url = JOptionPane.showInputDialog("Please specify a socket to connect with:", "socket://can0@192.168.30.129:29536");
 
         if (url != null) {
             BusURL beacon = BusURL.fromString(url);
